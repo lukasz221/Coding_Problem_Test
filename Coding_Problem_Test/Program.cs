b5e2a68136223d5743f260ea28ab2c416772092f
Łukasz Zalewski <lukaszzalewski1820@wp.pl>
2021-07-23 15:07:42 +0200

    True

--- Coding_Problem_Test/Program.cs
+++ Coding_Problem_Test/Program.cs
@@ -1,52 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Coding_Problem_Test
 {
     class Program
     {
-        public static int arraySize = 40000;
-        public static int[] array = new int[arraySize];
-        public static int k = 17;
-        public static int countKNumbers = 0;
-
-        public static int Min = 0;
-        public static int Max = 10;
-
         static void Main(string[] args)
         {
-            RandomArray();
-            var watch = Stopwatch.StartNew();
-            Check();
-            watch.Stop();
-            Console.WriteLine("Execution for " + arraySize + " random items: " + countKNumbers + " elapsed time: "+ watch.Elapsed + " seconds.");
-            Console.ReadKey();
+            Console.WriteLine(FindK().ToString());
         }
 
-        public static void Check()
+        public static bool FindK()
         {
-            int result;
-            
-            for (int i = 0; i < array.Length; i++)
+            bool check = false;
+            int k = 17;
+            var list = new List<int> { 10, 15, 3, 7 };
+
+            foreach (var item in list)
             {
-                for (int j = 1; j < array.Length; j++)
+                check = list.Contains(k - item);
+                if (check)
                 {
-                    result = array[i] + array[j];
-                    if (result == k)
-                    {
-                        countKNumbers += 1;
-                    }
+                    return check;
                 }
             }
-        }
-
-        public static void RandomArray()
-        {
-            Random randNum = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = randNum.Next(Min, Max);
-            }
+            return check;
         }
     }
 }
