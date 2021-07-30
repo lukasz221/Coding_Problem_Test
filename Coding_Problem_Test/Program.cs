16e3adec20a114fe7275bcbcc1b51f55a4b46e19
Łukasz Zalewski <lukaszzalewski1820@wp.pl>
2021-07-30 16:04:28 +0200

    True

--- Coding_Problem_Test/Program.cs
+++ Coding_Problem_Test/Program.cs
@@ -1,31 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Coding_Problem_Test
 {
     class Program
     {
+        public static int arraySize = 40000;
+        public static int[] array = new int[arraySize];
+        public static int k = 17;
+
+        public static int countKNumbers1 = 0;
+        public static int countKNumbers2 = 0;
+
+        public static int[] smallArray = new int[] { 11, 2, 8, 11, 9,};
+
+        public static int Min = 0;
+        public static int Max = 10;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine(FindK().ToString());
+            RandomArray();
+
+            var watch1 = Stopwatch.StartNew();
+
+            var r1 = Method1(smallArray, k);
+            watch1.Stop();
+            Console.WriteLine("Execution for Method 1 array size: " + smallArray.Length + " items: " + r1.ToString() + " elapsed time: " + watch1.Elapsed + " seconds.");
+            
+            var watch2 = Stopwatch.StartNew();
+
+            var r2 = Method2(smallArray, k);
+            watch2.Stop();
+            Console.WriteLine("Execution for Method 2 array size: " + smallArray.Length + " items: " + r2.ToString() + " elapsed time: " + watch2.Elapsed + " seconds.");
+
+            Console.ReadKey();
         }
 
-        public static bool FindK()
+        public static bool Method1(int[] array, int k)
         {
             bool check = false;
-            int k = 17;
-            var list = new List<int> { 10, 15, 3, 7 };
 
-            foreach (var item in list)
+            List<int> arrList = array.OfType<int>().ToList();
+
+            foreach (var item in arrList)
             {
-                check = list.Contains(k - item);
+                check = arrList.Contains(k - item);
+
                 if (check)
                 {
-                    return check;
+                    return true;
                 }
             }
-            return check;
+            return false;
+        }
+
+        public static bool Method2(int[] array, int k)
+        {
+            int result;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    result = array[i] + array[j];
+
+                    if (result == k)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static void RandomArray()
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randNum.Next(Min, Max);
+            }
         }
     }
 }
